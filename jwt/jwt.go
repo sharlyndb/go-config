4316818710c6494dc24e9a74507f43b9175bd560
coseman <904497924@qq.com>
2022-02-20 00:11:52 +0800

    新增ftp文件服务器相关配置

---
+++ jwt/jwt.go
@@ -0,0 +1,16 @@
+package jwt
+
+type JWT struct {
+
+	/** jwt签名 */
+	SigningKey     string `mapstructure:"signing-key"     json:"signingKey"      yaml:"signing-key"`
+
+	/** 过期时间 */
+	ExpiresTime    int64  `mapstructure:"expires-time"    json:"expiresTime"     yaml:"expires-time"`
+
+	/** 缓冲时间 */
+	BufferTime     int64  `mapstructure:"buffer-time"     json:"bufferTime"      yaml:"buffer-time"`
+
+	/** 多地登录拦截 true 拦截 fasle 不拦截 */
+	UseMultipoint  bool   `mapstructure:"use-multipoint"  json:"useMultipoint"   yaml:"use-multipoint"`
+}
