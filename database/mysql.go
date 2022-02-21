739341550e2f70205e28e9c22762387e3e02ce12
coseman <904497924@qq.com>
2022-02-21 21:05:20 +0800

    修改了数据库配置的格式

--- database/mysql.go
+++ database/mysql.go
@@ -9,27 +9,27 @@ package database
 type MySQL struct {
 
 	/** 数据库IP地址 */
-	Host         string       `mapstructure:"host"                json:"host"              yaml:"host"`
+	Host               string       `mapstructure:"host"                json:"host"              yaml:"host"`
 
 	/** 端口 */
-	Port         string       `mapstructure:"port"                json:"port"              yaml:"port"`
+	Port               string       `mapstructure:"port"                json:"port"              yaml:"port"`
 
 	/** 后缀配置  默认配置 charset=utf8mb4&parseTime=True&loc=Local */
-	Config       string       `mapstructure:"config"              json:"config"            yaml:"config"`
+	Config             string       `mapstructure:"config"              json:"config"            yaml:"config"`
 
 	/** 数据库名称 */
-	Dbname       string `mapstructure:"db-name"             json:"dbname"            yaml:"db-name"`
+	Dbname             string       `mapstructure:"db-name"             json:"dbname"            yaml:"db-name"`
 
 	/** 数据库用户名 */
-	Username     string `mapstructure:"username"            json:"username"          yaml:"username"`
+	Username           string       `mapstructure:"username"            json:"username"          yaml:"username"`
 
 	/** 数据库密码 */
-	Password     string `mapstructure:"password"            json:"password"          yaml:"password"`
+	Password           string       `mapstructure:"password"            json:"password"          yaml:"password"`
 
 	/** 最大空闲连接数 */
-	MaxIdleConns int    `mapstructure:"max-idle-conns"      json:"maxIdleConns"      yaml:"max-idle-conns"`
+	MaxIdleConns       int          `mapstructure:"max-idle-conns"      json:"maxIdleConns"      yaml:"max-idle-conns"`
 
 	/** 最大连接数 */
-	MaxOpenConns int    `mapstructure:"max-open-conns"      json:"maxOpenConns"      yaml:"max-open-conns"`
+	MaxOpenConns       int          `mapstructure:"max-open-conns"      json:"maxOpenConns"      yaml:"max-open-conns"`
 
 }
--- database/sqlite.go
+++ database/sqlite.go
@@ -9,14 +9,14 @@ package database
 type SQLite struct {
 
 	/** sqlite 文件存放位置 */
-	DbPath       string `mapstructure:"db-path"             json:"dbpath"            yaml:"db-path"`
+	DbPath             string       `mapstructure:"db-path"             json:"dbpath"            yaml:"db-path"`
 
 	/** 最大空闲连接数 */
-	MaxIdleConns int    `mapstructure:"max-idle-conns"      json:"maxIdleConns"      yaml:"max-idle-conns"`
+	MaxIdleConns       int          `mapstructure:"max-idle-conns"      json:"maxIdleConns"      yaml:"max-idle-conns"`
 
 	/** 最大连接数 */
-	MaxOpenConns int    `mapstructure:"max-open-conns"      json:"maxOpenConns"      yaml:"max-open-conns"`
+	MaxOpenConns       int          `mapstructure:"max-open-conns"      json:"maxOpenConns"      yaml:"max-open-conns"`
 
 	/** 是否执行清除命令 */
-	Vacuum       bool   `mapstructure:"vacuum"              json:"vacuum"            yaml:"vacuum"`
+	Vacuum             bool         `mapstructure:"vacuum"              json:"vacuum"            yaml:"vacuum"`
 }
