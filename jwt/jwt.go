84a38f3aed1fdd63eaa88bfa80d2e999eb7f401f
coseman <904497924@qq.com>
2022-02-21 21:07:31 +0800

    修改了 jwt 结构体的格式

--- jwt/jwt.go
+++ jwt/jwt.go
@@ -8,14 +8,14 @@ package jwt
 type JWT struct {
 
 	/** jwt签名 */
-	SigningKey     string `mapstructure:"signing-key"     json:"signingKey"      yaml:"signing-key"`
+	SigningKey        string      `mapstructure:"signing-key"         json:"signingKey"        yaml:"signing-key"`
 
 	/** 过期时间 */
-	ExpiresTime    int64  `mapstructure:"expires-time"    json:"expiresTime"     yaml:"expires-time"`
+	ExpiresTime       int64       `mapstructure:"expires-time"        json:"expiresTime"       yaml:"expires-time"`
 
 	/** 缓冲时间 */
-	BufferTime     int64  `mapstructure:"buffer-time"     json:"bufferTime"      yaml:"buffer-time"`
+	BufferTime        int64       `mapstructure:"buffer-time"         json:"bufferTime"        yaml:"buffer-time"`
 
 	/** 多地登录拦截 true 拦截 fasle 不拦截 */
-	UseMultipoint  bool   `mapstructure:"use-multipoint"  json:"useMultipoint"   yaml:"use-multipoint"`
+	UseMultipoint     bool        `mapstructure:"use-multipoint"      json:"useMultipoint"     yaml:"use-multipoint"`
 }
