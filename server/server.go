714876102b1413b20cfdd361b5c0577a5f277e76
coseman <904497924@qq.com>
2022-02-21 23:37:40 +0800

    新增服务配置

--- server/server.go
+++ server/server.go
@@ -11,16 +11,13 @@ type Server struct {
 	Addr                      int         `mapstructure:"addr"                        json:"addr"                     yaml:"addr"`
 
 	/** 服务名称 */
-	ServiceName               string      `mapstructure:"service-name"                json:"serviceName"              yaml:"service-name"`
+	ServerName                string      `mapstructure:"server-name"                 json:"serverName"               yaml:"server-name"`
 
 	/** 请求根路径 */
 	ContextPath               string      `mapstructure:"context-path"                json:"contextPath"              yaml:"context-path"`
 
 	/** 数据库类型 */
 	DataDriver                string      `mapstructure:"data-driver"                 json:"dataDriver"               yaml:"data-driver"`
-
-	/** 日志打印级别 */
-	LogLevel                  string      `mapstructure:"log-level"                   json:"logLevel"                 yaml:"log-level"`
 
 	/** 是否开启请求方式检测 */
 	HandleMethodNotAllowed    bool        `mapstructure:"handle-method-not-allowed"   json:"handleMethodNotAllowed"   yaml:"handle-method-not-allowed"`
