191e428403566ce8718c69ecf10d82a6ce0d5b27
coseman <904497924@qq.com>
2022-02-28 16:46:42 +0800

    更改了数据库连接配置

--- database/sqlite.go
+++ database/sqlite.go
@@ -9,17 +9,23 @@ package database
 type SQLite struct {
 
 	/** sqlite 文件存放位置 */
-	DbPath             string       `mapstructure:"db-path"             json:"dbpath"            yaml:"db-path"`
+	DbPath                string          `mapstructure:"db-path"                  json:"dbpath"                   yaml:"db-path"`
 
 	/** 最大空闲连接数 */
-	MaxIdleConns       int          `mapstructure:"max-idle-conns"      json:"maxIdleConns"      yaml:"max-idle-conns"`
+	MaxIdleConns          int             `mapstructure:"max-idle-conns"           json:"maxIdleConns"             yaml:"max-idle-conns"`
 
 	/** 最大连接数 */
-	MaxOpenConns       int          `mapstructure:"max-open-conns"      json:"maxOpenConns"      yaml:"max-open-conns"`
+	MaxOpenConns          int             `mapstructure:"max-open-conns"           json:"maxOpenConns"             yaml:"max-open-conns"`
 
 	/** sql日志等级 */
-	LogLevel           string       `mapstructure:"log-level"           json:"logLevel"          yaml:"log-level"`
+	LogLevel              string          `mapstructure:"log-level"                json:"logLevel"                 yaml:"log-level"`
 
 	/** 是否执行清除命令 */
-	Vacuum             bool         `mapstructure:"vacuum"              json:"vacuum"            yaml:"vacuum"`
+	Vacuum                bool            `mapstructure:"vacuum"                   json:"vacuum"                   yaml:"vacuum"`
+
+	/** 连接最大空闲时间  单位：秒 */
+	ConnMaxIdleTime       int             `mapstructure:"conn-max-idle-time"       json:"connMaxIdleTime"          yaml:"conn-max-idle-time"`
+
+	/** 连接最大生命周期  单位：秒 */
+	ConnMaxLifetime       int             `mapstructure:"conn-max-life-time"       json:"connMaxLifetime"          yaml:"conn-max-life-time"`
 }
